09773f93ddedc89767aafa0537706237cffcb26a
gabrielbsb21 <gabrielbsb21@outlook.com>
2023-03-02 21:19:35 -0300

    begin a new project

---
+++ guessing_game/target/debug/deps/guessing_game-5d5c49fb0bea789d.d
@@ -0,0 +1,5 @@
+/home/gabrielteixeira/Github/aprendendo_rust/guessing_game/target/debug/deps/guessing_game-5d5c49fb0bea789d.rmeta: src/main.rs
+
+/home/gabrielteixeira/Github/aprendendo_rust/guessing_game/target/debug/deps/guessing_game-5d5c49fb0bea789d.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/deps/guessing_game-df6a4be391a7114a.d
@@ -0,0 +1,5 @@
+/home/gabrielteixeira/Github/aprendendo_rust/guessing_game/target/debug/deps/guessing_game-df6a4be391a7114a: src/main.rs
+
+/home/gabrielteixeira/Github/aprendendo_rust/guessing_game/target/debug/deps/guessing_game-df6a4be391a7114a.d: src/main.rs
+
+src/main.rs:
